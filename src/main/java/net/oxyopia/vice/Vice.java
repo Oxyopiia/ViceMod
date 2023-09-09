d3fc4e53e65962c7e99a07fa1f53ce6b9daefa0e
Oxyopiia <noahharryb@gmail.com>
2023-09-09 13:38:18 +0100

    Add Vigilance config defaults

--- src/main/java/net/oxyopia/vice/Vice.java
+++ src/main/java/net/oxyopia/vice/Vice.java
@@ -1,10 +1,15 @@
 package net.oxyopia.vice;
 
+import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
 
 public class Vice implements ClientModInitializer {
+	private static final Logger LOGGER = LogUtils.getLogger();
+
 	@Override
 	public void onInitializeClient() {
-		System.out.println("Vice ClientMod Loaded!");
+
 	}
+
 }
---
+++ src/main/java/net/oxyopia/vice/commands/BaseCommand.java
@@ -0,0 +1,5 @@
+package net.oxyopia.vice.commands;
+
+public class BaseCommand {
+    // Work on this soon
+}

97d727b515d44a72b0e01997578c44b839d486bf
Oxyopiia <noahharryb@gmail.com>
2024-01-17 00:21:58 +0000

    Add EntityGlowEvent

--- src/main/java/net/oxyopia/vice/mixin/MixinMinecraftClient.java
+++ src/main/java/net/oxyopia/vice/mixin/MixinMinecraftClient.java
@@ -1,7 +1,10 @@
 package net.oxyopia.vice.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.oxyopia.vice.events.ClientTickEvent;
+import net.oxyopia.vice.events.EntityGlowEvent;
 import net.oxyopia.vice.events.LeftClickEvent;
 import net.oxyopia.vice.events.RightClickEvent;
 import net.oxyopia.vice.utils.Utils;
@@ -9,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -39,5 +43,24 @@ public class MixinMinecraftClient {
 			tickTotal++;
 			EVENT_MANAGER.publish(new ClientTickEvent(tickTotal));
 		}
+	}
+
+	@Redirect(at = @At(value="INVOKE", target="Lnet/minecraft/entity/Entity;isGlowing()Z"), method = "hasOutline")
+	private boolean updateGlowing(Entity entity) {
+		if (Utils.INSTANCE.getInDoomTowers()) {
+			MinecraftClient client = MinecraftClient.getInstance();
+			boolean shouldGlowDefault = entity.isGlowing() || client.player != null && client.player.isSpectator() && client.options.spectatorOutlinesKey.isPressed() && entity.getType() == EntityType.PLAYER;
+
+			Object result = EVENT_MANAGER.publish(new EntityGlowEvent(entity));
+
+			if (result instanceof Boolean) {
+				return (Boolean) result;
+			}
+
+			return shouldGlowDefault;
+		}
+
+
+		return false;
 	}
 }

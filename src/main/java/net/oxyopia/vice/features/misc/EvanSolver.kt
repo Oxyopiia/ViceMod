3af6e199fcd9aac173348a6c905eba99b1f1880e
Derpy666 <noamgl2002@gmail.com>
2024-02-27 11:10:34 +0200

    Added more questions

--- src/main/java/net/oxyopia/vice/features/misc/EvanSolver.kt
+++ src/main/java/net/oxyopia/vice/features/misc/EvanSolver.kt
@@ -35,6 +35,7 @@ object EvanSolver {
 		"Is the Jelly NPC on Floor 4?" to true,
 		"Is Exonitas after World 11?" to true,
 		"Is \"Chef\" a real set?" to true,
+		"Do Pistons heal 2 hearts?" to true,
 
 		"Was DoomTowers made on the 6th May 2023?" to false,
 		"Is Exonitas after World 12?" to false,
@@ -46,16 +47,17 @@ object EvanSolver {
 		"Is World 10 called \"Arcade\"" to false,
 		"Is the Jelly NPC on Floor 5?" to false,
 		"Is the max Carnage Level 4?" to false,
+		"Is the max Carnage Level 5?" to false,
 		"The answer is not True" to false,
 		"Is the Adventurer's Hook from World 4?" to false,
-		"Is the max Carnage Level 5?" to false,
 		"Is the max amount of silver 850?" to false,
 		"Does the World 11 Train spawn every 40m?" to false,
 		"Is the \"Whopper\" a World 4 Recipe?" to false,
 		"Is the \"Whoppa\" a World 4 Recipe?" to false,
 		"Are there 6 Mystic Items in World 5?" to false,
+		"What colour am I?" to false,
 		"Is \"Glitch\" a real set?" to false,
-		"What colour am I?" to false
+		"Is \"Carnage\" a real set?" to false,
 	)
 		get() = field.mapKeys { (key, _) -> key.lowercase() }
 }

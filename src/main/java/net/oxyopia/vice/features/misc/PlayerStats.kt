69e262bf364193ed23149ac4497c24219717b603
Derpy666 <noamgl2002@gmail.com>
2024-02-27 12:45:07 +0200

    Added "Fish Time" to PlayerStats

--- src/main/java/net/oxyopia/vice/features/misc/PlayerStats.kt
+++ src/main/java/net/oxyopia/vice/features/misc/PlayerStats.kt
@@ -9,19 +9,23 @@ import net.oxyopia.vice.events.HudRenderEvent
 import net.oxyopia.vice.events.core.SubscribeEvent
 import net.oxyopia.vice.utils.HudUtils.drawStrings
 import net.oxyopia.vice.utils.ItemUtils.getLore
+import net.oxyopia.vice.utils.ItemUtils.nameWithoutEnchants
 
 object PlayerStats : HudElement("Player Stats", Vice.storage.misc.playerStatsPos) {
     @SubscribeEvent
     fun onHudRender(event: HudRenderEvent) {
-        if(!Vice.config.PLAYER_STATS) return
+        if (!Vice.config.PLAYER_STATS) return
 
         val defenseRegex = Regex("Defence: ([+-]?\\d+)")
         val speedRegex = Regex("Speed: ([+-]?\\d+(?:\\.\\d+)?)%")
+        val fishTimeRegex = Regex("Fish Time: (\\d+)-(\\d+(\\.\\d+)?)s")
+        val fishReduceTimeRegex = Regex("Fish Time: -?(\\d+(?:\\.\\d+)?)s")
 
         val player = MinecraftClient.getInstance().player
 
         var defence = 0
         var speed = 0f
+        var fishReduce = 0f
         val movementSpeed = player?.movementSpeed ?: 1f
 
         player?.armorItems?.forEach { itemStack ->
@@ -35,6 +39,37 @@ object PlayerStats : HudElement("Player Stats", Vice.storage.misc.playerStatsPos
                 speedRegex.find(line)?.apply {
                     speed += groupValues[1].toFloatOrNull() ?: 0f
                 }
+
+                fishReduceTimeRegex.find(line)?.apply {
+                    fishReduce -= groupValues[1].toFloat()
+                }
+            }
+        }
+
+        var fishTimeNum = (0 - (fishReduce * -1))
+
+        var fishTime = "${fishTimeNum}s"
+
+        val itemStack = player?.mainHandStack
+        val itemName = itemStack?.nameWithoutEnchants()
+
+        if (itemName != null) {
+            if (itemName.contains("Basic Fishing Rod") ||
+                itemName.contains("Reinforced Fishing Rod") ||
+                itemName.contains("Frigid Fishing Rod") ||
+                itemName.contains("Polar Rod") ||
+                itemName.contains("Gilded Fishing Rod") ||
+                itemName.contains("RGB Rod")
+            ) {
+                val lore = itemStack.getLore()
+
+                lore.forEach { line ->
+                    fishTimeRegex.find(line)?.apply {
+                        fishTime = "${groupValues[1]}-${groupValues[2].toFloat() - (fishReduce * -1)}s"
+                    }
+                }
+            } else {
+                fishTime = "Hold Fishing Rod"
             }
         }
 
@@ -43,6 +78,7 @@ object PlayerStats : HudElement("Player Stats", Vice.storage.misc.playerStatsPos
         list.add("&&b&&lPlayer Stats")
         list.add("&&fDefence: &&a\uD83D\uDEE1 $defence")
         list.add("&&fSpeed: &&e⚡ $speed% &&7(${String.format("%.2f", movementSpeed * 100).toFloat()})")
+        list.add("&&fFish Time: &&b\uD83D\uDD51 $fishTime")
 
         position.drawStrings(list, event.context)
     }
@@ -58,7 +94,8 @@ object PlayerStats : HudElement("Player Stats", Vice.storage.misc.playerStatsPos
         val list = listOf(
             "&&b&&lPlayer Stats",
             "&&fDefence: &&a\uD83D\uDEE1 16",
-            "&&fSpeed: &&e⚡ 30.0% &&7(19.5)"
+            "&&fSpeed: &&e⚡ 30.0% &&7(19.5)",
+            "&&fFish Time: &&b\uD83D\uDD51 5-20s"
         )
 
         return position.drawStrings(list, context)

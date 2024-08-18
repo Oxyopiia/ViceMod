bc37e62b2f43312fb63a23a9ed771a1b4c82f903
Oxyopiia <101736939+Oxyopiia@users.noreply.github.com>
2024-08-18 16:41:45 +0100

    Code cleanup

--- src/main/java/net/oxyopia/vice/utils/Utils.kt
+++ src/main/java/net/oxyopia/vice/utils/Utils.kt
@@ -1,11 +1,8 @@
 package net.oxyopia.vice.utils
 
-import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.network.ClientPlayerEntity
 import net.minecraft.client.world.ClientWorld
-import net.minecraft.nbt.NbtCompound
-import net.minecraft.nbt.StringNbtReader
 import net.oxyopia.vice.Vice
 import net.oxyopia.vice.data.World
 
@@ -27,26 +24,10 @@ object Utils {
 
 	fun net.minecraft.world.World.name(): String = registryKey.value.path
 
-	fun parseNbt(nbt: String): NbtCompound? {
-		try {
-			return StringNbtReader.parse(nbt)
-		} catch (e: CommandSyntaxException) {
-			DevUtils.sendErrorMessage(e, "An error occurred parsing an item's NBT!")
-		}
-		return null
-	}
-
 	/**
 	 * Converts any two consecutive ampersands in a string to a section character, useful for formatting Minecraft Texts
 	 */
 	fun String.convertFormatting(): String {
 		return this.replace("&&", "§")
-	}
-
-	/**
-	 * Removes any two consecutive ampersands, or sections signs, which are followed by a character in a string
-	 */
-	fun String.removeFormatting(): String {
-		return this.replace("(&&|§)[0-9a-fk-or]", "", ignoreCase = true)
 	}
 }

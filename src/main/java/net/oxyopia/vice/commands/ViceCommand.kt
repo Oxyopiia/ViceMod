38cc8f7b5d305a8c89b092aa6131b59a35d5fa82
Oxyopiia <101736939+Oxyopiia@users.noreply.github.com>
2025-01-14 21:06:14 +0000

    Add /vice config, Code cleanup

--- src/main/java/net/oxyopia/vice/commands/ViceCommand.kt
+++ src/main/java/net/oxyopia/vice/commands/ViceCommand.kt
@@ -3,9 +3,11 @@ package net.oxyopia.vice.commands
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.arguments.FloatArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import gg.essential.universal.UScreen
 import gg.essential.universal.wrappers.message.UTextComponent
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 import net.minecraft.client.MinecraftClient
 import net.minecraft.item.Items
 import net.minecraft.text.ClickEvent
@@ -37,108 +39,127 @@ object ViceCommand {
 					MinecraftClient.getInstance().send(Runnable { UScreen.displayScreen(Vice.config.gui()) })
 					Command.SINGLE_SUCCESS
 				}
+				.then(ClientCommandManager.literal("config")
+					.executes {
+						MinecraftClient.getInstance().send(Runnable { UScreen.displayScreen(Vice.config.gui()) })
+						Command.SINGLE_SUCCESS
+					})
 				.then(ClientCommandManager.literal("dev")
 					.executes {
 						MinecraftClient.getInstance().send(Runnable { UScreen.displayScreen(Vice.devConfig.gui()) })
 						Command.SINGLE_SUCCESS
 					}
-					.then(ClientCommandManager.literal("toggle")
-						.executes {
-							Vice.config.DEVMODE = !Vice.config.DEVMODE
-							Vice.config.markDirty()
+					.addDevSubcommands()
+				)
+		)
+	}
 
-							val status = if (Vice.config.DEVMODE) "§aenabled" else "§cdisabled"
-							ChatUtils.sendViceMessage("§7Dev mode: $status")
-							Command.SINGLE_SUCCESS
-						}
-					)
-					.then(ClientCommandManager.literal("converthex")
-						.then(ClientCommandManager.argument("hex", StringArgumentType.word())
-							.executes {
-								val hex = StringArgumentType.getString(it, "hex")
-								try {
-									var formattedHex = hex
-									if(formattedHex.startsWith("#")) formattedHex = formattedHex.replace("#", "")
-									if(!formattedHex.startsWith("0x")) formattedHex = "0x$hex"
-									val color = Color.decode(formattedHex)
+	private fun LiteralArgumentBuilder<FabricClientCommandSource?>.addDevSubcommands(): LiteralArgumentBuilder<FabricClientCommandSource?> {
+		return this
+			.then(ClientCommandManager.literal("toggle")
+				.executes {
+					Vice.config.DEVMODE = !Vice.config.DEVMODE
+					Vice.config.markDirty()
 
-									ChatUtils.sendViceMessage(
-										UTextComponent("Converted hex ".toText().append(hex.toText(color)).append(" to rgb: ".toText(Colors.ChatColor.Grey)).append("Color(${color.red},${color.green},${color.blue})".toText(color)))
-											.setClick(ClickEvent.Action.COPY_TO_CLIPBOARD, "(${color.red},${color.green},${color.blue})")
-											.setHover(HoverEvent.Action.SHOW_TEXT, "Click to copy color".toText(color))
-									)
-								} catch (e: NumberFormatException) {
-									ChatUtils.sendViceMessage("Invalid hex string: $hex".toText(Colors.ChatColor.Red))
-								}
-								Command.SINGLE_SUCCESS
-							}
-						)
-					)
-					.then(ClientCommandManager.literal("tree")
-						.executes {
-							val subscribers = EVENT_MANAGER.subscribers
-							ChatUtils.sendViceMessage("&&e&&lVICE EVENT TREE")
+					val status = if (Vice.config.DEVMODE) "§aenabled" else "§cdisabled"
+					ChatUtils.sendViceMessage("§7Dev mode: $status")
+					Command.SINGLE_SUCCESS
+				}
+			)
+			.then(ClientCommandManager.literal("converthex")
+				.then(ClientCommandManager.argument("hex", StringArgumentType.word())
+					.executes {
+						val hex = StringArgumentType.getString(it, "hex")
+						try {
+							var formattedHex = hex
+							if (formattedHex.startsWith("#")) formattedHex = formattedHex.replace("#", "")
+							if (!formattedHex.startsWith("0x")) formattedHex = "0x$hex"
+							val color = Color.decode(formattedHex)
 
-							subscribers.forEach {(event, listeners) ->
-								ChatUtils.sendViceMessage("&&a${event.name.removePrefix("net.oxyopia.")}")
-
-								listeners.forEach { listener ->
-									val classText = listener.source.javaClass.packageName.removePrefix("net.oxyopia.") + ".&&b" + listener.source.javaClass.simpleName
-
-									ChatUtils.sendViceMessage("&&f\\- &&8${classText}&&8/${listener.target.name}()")
-								}
-							}
-
-							Command.SINGLE_SUCCESS
-						}
-					)
-					.then(ClientCommandManager.literal("data")
-						.executes {
-							sendDevData()
-							Command.SINGLE_SUCCESS
-						}
-					)
-					.then(ClientCommandManager.literal("message")
-						.then(ClientCommandManager.argument("message", StringArgumentType.greedyString())
-							.executes {
-								val message = StringArgumentType.getString(it, "message")
-
-								EVENT_MANAGER.publish(ChatEvent(Text.of(message)))
-								ChatUtils.sendViceMessage("§aSent ChatEvent!")
-								Command.SINGLE_SUCCESS
-							}
-						)
-					)
-					.then(ClientCommandManager.literal("sound")
-						.then(ClientCommandManager.argument("sound", StringArgumentType.word())
-							.then(ClientCommandManager.argument("volume", FloatArgumentType.floatArg(0f))
-								.then(ClientCommandManager.argument("pitch", FloatArgumentType.floatArg(0f, 2f))
-									.executes {
-										val soundName = StringArgumentType.getString(it, "sound")
-										val pitch = FloatArgumentType.getFloat(it, "pitch")
-										val volume = FloatArgumentType.getFloat(it, "volume")
-										EVENT_MANAGER.publish(SoundEvent(soundName, pitch, volume))
-										SoundUtils.playSound(soundName, pitch, volume)
-										ChatUtils.sendViceMessage("§aSent SoundEvent! §7($soundName, §d$pitch, §e$volume§7)")
-										Command.SINGLE_SUCCESS
-									}
+							ChatUtils.sendViceMessage(
+								UTextComponent(
+									"Converted hex ".toText().append(hex.toText(color))
+										.append(" to rgb: ".toText(Colors.ChatColor.Grey))
+										.append("Color(${color.red},${color.green},${color.blue})".toText(color))
 								)
+									.setClick(
+										ClickEvent.Action.COPY_TO_CLIPBOARD,
+										"(${color.red},${color.green},${color.blue})"
+									)
+									.setHover(HoverEvent.Action.SHOW_TEXT, "Click to copy color".toText(color))
 							)
-						)
-					)
-					.then(ClientCommandManager.literal("actionbar")
-						.then(ClientCommandManager.argument("text", StringArgumentType.greedyString())
+						} catch (e: NumberFormatException) {
+							ChatUtils.sendViceMessage("Invalid hex string: $hex".toText(Colors.ChatColor.Red))
+						}
+						Command.SINGLE_SUCCESS
+					}
+				)
+			)
+			.then(ClientCommandManager.literal("tree")
+				.executes {
+					val subscribers = EVENT_MANAGER.subscribers
+					ChatUtils.sendViceMessage("&&e&&lVICE EVENT TREE")
+
+					subscribers.forEach { (event, listeners) ->
+						ChatUtils.sendViceMessage("&&a${event.name.removePrefix("net.oxyopia.")}")
+
+						listeners.forEach { listener ->
+							val classText =
+								listener.source.javaClass.packageName.removePrefix("net.oxyopia.") + ".&&b" + listener.source.javaClass.simpleName
+
+							ChatUtils.sendViceMessage("&&f\\- &&8${classText}&&8/${listener.target.name}()")
+						}
+					}
+
+					Command.SINGLE_SUCCESS
+				}
+			)
+			.then(ClientCommandManager.literal("data")
+				.executes {
+					sendDevData()
+					Command.SINGLE_SUCCESS
+				}
+			)
+			.then(ClientCommandManager.literal("message")
+				.then(ClientCommandManager.argument("message", StringArgumentType.greedyString())
+					.executes {
+						val message = StringArgumentType.getString(it, "message")
+
+						EVENT_MANAGER.publish(ChatEvent(Text.of(message)))
+						ChatUtils.sendViceMessage("§aSent ChatEvent!")
+						Command.SINGLE_SUCCESS
+					}
+				)
+			)
+			.then(ClientCommandManager.literal("sound")
+				.then(ClientCommandManager.argument("sound", StringArgumentType.word())
+					.then(ClientCommandManager.argument("volume", FloatArgumentType.floatArg(0f))
+						.then(ClientCommandManager.argument("pitch", FloatArgumentType.floatArg(0f, 2f))
 							.executes {
-								val message = StringArgumentType.getString(it, "text").toText()
-								EVENT_MANAGER.publish(ActionBarEvent(message))
-								HudUtils.sendVanillaActionBar(message)
-								ChatUtils.sendViceMessage("§aSent ActionBarEvent!")
+								val soundName = StringArgumentType.getString(it, "sound")
+								val pitch = FloatArgumentType.getFloat(it, "pitch")
+								val volume = FloatArgumentType.getFloat(it, "volume")
+								EVENT_MANAGER.publish(SoundEvent(soundName, pitch, volume))
+								SoundUtils.playSound(soundName, pitch, volume)
+								ChatUtils.sendViceMessage("§aSent SoundEvent! §7($soundName, §d$pitch, §e$volume§7)")
 								Command.SINGLE_SUCCESS
 							}
 						)
 					)
 				)
-		)
+			)
+			.then(ClientCommandManager.literal("actionbar")
+				.then(ClientCommandManager.argument("text", StringArgumentType.greedyString())
+					.executes {
+						val message = StringArgumentType.getString(it, "text").toText()
+						EVENT_MANAGER.publish(ActionBarEvent(message))
+						HudUtils.sendVanillaActionBar(message)
+						ChatUtils.sendViceMessage("§aSent ActionBarEvent!")
+						Command.SINGLE_SUCCESS
+					}
+				)
+			)
+
 	}
 
 	private fun sendDevData() {

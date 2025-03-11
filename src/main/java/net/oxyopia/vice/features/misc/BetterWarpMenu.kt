a6c7390dd3fedeb996111fec6c30f56e5884ed69
Derpy <37512753+Derpy666@users.noreply.github.com>
2025-03-11 19:41:14 +0200

    Add Floor 9 data to warps (#119)

--- src/main/java/net/oxyopia/vice/features/misc/BetterWarpMenu.kt
+++ src/main/java/net/oxyopia/vice/features/misc/BetterWarpMenu.kt
@@ -1,5 +1,6 @@
 package net.oxyopia.vice.features.misc
 
+import net.minecraft.item.Items
 import net.minecraft.text.MutableText
 import net.minecraft.text.Text
 import net.oxyopia.vice.Vice
@@ -25,6 +26,7 @@ object BetterWarpMenu {
 		listOf("Showdown".toText(Colors.Showdown), "The Journey to the Glitch HQ".toText(Colors.GlitchHQ)),
 		listOf("Lost In Time".toText(Colors.LostInTime), "Soulswift Sands".toText(Colors.LostInTime), "Timeless Tastes".toText(Colors.Showdown), "Cosmic Collapse".toText(Colors.SpaceEscape)),
 		listOf("Starry Streets".toText(Colors.StarryStreets), "On the Run".toText(Colors.OnTheRun)),
+		listOf("Oracle".toText(Colors.Oracle), "Bound to Sin".toText(Colors.BoundToSin))
 	)
 	private val bosses: List<MutableText> = listOf(
 		"Vice".toText(Colors.ViceBoss),
@@ -34,7 +36,8 @@ object BetterWarpMenu {
 		"Lifesteal Box SMP".toText(Colors.ChatColor.Aqua),
 		"Diox".toText(Colors.Diox),
 		"Elderpork".toText(Colors.Elderpork),
-		"THE VATICAN".toText(Colors.Vatican)
+		"THE VATICAN".toText(Colors.Vatican),
+		"Anodized".toText(Colors.Anodized)
 	)
 
 	@SubscribeEvent

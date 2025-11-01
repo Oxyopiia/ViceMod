9bb3963edfe1623e83d4c101d4495323db51e3db
Derpy <37512753+Derpy666@users.noreply.github.com>
2025-11-01 23:56:58 +0200

    Added Ivory Timer (#165)

--- src/main/java/net/oxyopia/vice/Vice.kt
+++ src/main/java/net/oxyopia/vice/Vice.kt
@@ -70,6 +70,7 @@ import net.oxyopia.vice.features.event.summer.SummerTimers
 import net.oxyopia.vice.features.hud.FishingBrewTimer
 import net.oxyopia.vice.features.event.turkinator.TurkinatorInvasionTimer
 import net.oxyopia.vice.features.hud.BoxtownTimer
+import net.oxyopia.vice.features.hud.IvoryTimer
 import net.oxyopia.vice.features.worlds.glitchhq.EvanNotification
 import net.oxyopia.vice.features.worlds.starrysuburbs.CheeseHelper
 import net.oxyopia.vice.features.worlds.starrysuburbs.FallenStarWaypoints
@@ -186,6 +187,7 @@ class Vice : ClientModInitializer {
 		EVENT_MANAGER.subscribe(BetterWarpMenu)
 		EVENT_MANAGER.subscribe(AmmoCounter)
 		EVENT_MANAGER.subscribe(BoxtownTimer)
+		EVENT_MANAGER.subscribe(IvoryTimer)
 
 		EVENT_MANAGER.subscribe(BossCounter)
 		EVENT_MANAGER.subscribe(FishingBrewTimer)

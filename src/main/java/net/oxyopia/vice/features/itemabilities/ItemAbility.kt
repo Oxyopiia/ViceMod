1628439a6e21df986151f17d6c3a97ea28e7dd99
Derpy666 <noamgl2002@gmail.com>
2025-11-01 23:17:53 +0200

    Added Zap Jevelin to ItemAbility

--- src/main/java/net/oxyopia/vice/features/itemabilities/ItemAbility.kt
+++ src/main/java/net/oxyopia/vice/features/itemabilities/ItemAbility.kt
@@ -73,6 +73,7 @@ enum class ItemAbility(
     STAR_LEECH("Star Leech", 10f, set = Set.TRICKSTER, setAmount = 1),
     TERMINUS_RAILGUN("Terminus' Railgun", 20f),
     BUG_ZAPPER("Bug Zapper", 15f),
+    ZAP_JAVELIN("Zap Javelin", 6f, set = Set.DIGITAL, setAmount = 3),
 
     // No Display
     LASER_POINT_MINIGUN("Laser Point Minigun", 0.5f, set = Set.HEAVY, setAmount = 2),
